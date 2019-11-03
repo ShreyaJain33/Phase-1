ca2768bfd21f2580b6b4ca59ae63b3c86408ca6d
ShreyaJain33 <50612608+ShreyaJain33@users.noreply.github.com>
2019-11-03 12:58:15 +0530

    Add files via upload

---
+++ ForeAer/functions.js
@@ -0,0 +1,112 @@
+var TxtType = function(el, toRotate, period) {
+        this.toRotate = toRotate;
+        this.el = el;
+        this.loopNum = 0;
+        this.period = parseInt(period, 10) || 2000;
+        this.txt = '';
+        this.tick();
+        this.isDeleting = false;
+    };
+
+    TxtType.prototype.tick = function() {
+        var i = this.loopNum % this.toRotate.length;
+        var fullTxt = this.toRotate[i];
+
+        if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+        } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+        }
+
+        this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+        var that = this;
+        var delta = 200 - Math.random() * 100;
+
+        if (this.isDeleting) { delta /= 2; }
+
+        if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period;
+        this.isDeleting = false; // to delete the sentence backward change value to true
+        } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
+        }
+
+        setTimeout(function() {
+        that.tick();
+        }, delta);
+    };
+
+    window.onload = function() {
+        var elements = document.getElementsByClassName('typewrite');
+        for (var i=0; i<elements.length; i++) {
+            var toRotate = elements[i].getAttribute('data-type');
+            var period = elements[i].getAttribute('data-period');
+            if (toRotate) {
+              new TxtType(elements[i], JSON.parse(toRotate), period);
+            }
+        }
+
+        var css = document.createElement("style");
+        css.type = "text/css";
+        css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #808080}";
+        document.body.appendChild(css);
+    };
+
+
+
+    function makeTimer() {
+
+		var endTime = new Date("20 November 2019 9:56:00 GMT+01:00");
+			endTime = (Date.parse(endTime) / 1000);
+
+			var now = new Date();
+			now = (Date.parse(now) / 1000);
+
+			var timeLeft = endTime - now;
+
+			var days = Math.floor(timeLeft / 86400);
+			var hours = Math.floor((timeLeft - (days * 86400)) / 3600);
+			var minutes = Math.floor((timeLeft - (days * 86400) - (hours * 3600 )) / 60);
+			var seconds = Math.floor((timeLeft - (days * 86400) - (hours * 3600) - (minutes * 60)));
+      //if (days < "10"){ days = "0" + days; }
+			if (hours < "10") { hours = "0" + hours; }
+			if (minutes < "10") { minutes = "0" + minutes; }
+			if (seconds < "10") { seconds = "0" + seconds; }
+
+			$("#days").html(days + '<span class="unit">Days</span>');
+			$("#hours").html(hours + '<span class="unit">Hours</span>');
+			$("#minutes").html(minutes + '<span class="unit">Minutes</span>');
+			$("#seconds").html(seconds + '<span class="unit">Seconds</span>');
+
+	}
+
+	setInterval(function() { makeTimer(); }, 1000);
+
+function ValidateEmail(inputText)
+  {
+  var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if(inputText.match(mailformat))
+  	return true;
+  else
+  	return false;
+  };
+
+
+function message(){
+  	inputText=document.getElementById("email").value;
+  	flag=ValidateEmail(inputText);
+  	if(flag==true)
+  	{
+  		document.getElementById("thanks").innerHTML = "Thanks for Subscribing."
+  		document.getElementById("subscribe_grp").style.display="none";
+  	}
+  	else
+  	{
+  		alert("You have entered an invalid email address!");
+  		document.getElementById("email").focus();
+  	}
+  };
+
